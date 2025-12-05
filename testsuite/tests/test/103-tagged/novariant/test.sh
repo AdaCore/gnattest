3c758b70ef47cb65448187acae00854dab675e5b
Dorian Peron <peron@adacore.com>
2025-12-05 12:22:53 +0100

    Deprecate passing a project file as a positional argument
    Use the `-P <file>` flag instead

--- testsuite/tests/test/103-tagged/novariant/test.sh
+++ testsuite/tests/test/103-tagged/novariant/test.sh
@@ -8,6 +8,6 @@ mkdir -p test/obj/gnattest/tests/JSON_Tests obj
 tgen_marshalling -P test.gpr --templates-dir=$TEMPLATES_PATH -o obj/tgen_support src/pkg.ads
 gprbuild -q -P obj/tgen_support/tgen_support.gpr
 
-gnattest test.gpr --gen-test-vectors
+gnattest -P test.gpr --gen-test-vectors
 gprbuild -P obj/gnattest/harness/test_driver.gpr -q
 ./obj/gnattest/harness/test_runner
