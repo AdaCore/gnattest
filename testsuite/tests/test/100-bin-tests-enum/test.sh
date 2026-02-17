582ffa0bd2174c67d3da844cb3b4b214994aae7c
Léo Creuse <creuse@adacore.com>
2026-02-17 18:50:48 +0100

    TGen: Ensure the name of all binary tests are consistent
    The filenames of the tests generated with --gen-test-vectors used to
    differ from those generated through --dump-test-inputs.
    
    This has been harmonized, now the filename only consists of the simple
    name of the subprogram and the hash to disambiguate overloads.

--- testsuite/tests/test/100-bin-tests-enum/test.sh
+++ testsuite/tests/test/100-bin-tests-enum/test.sh
@@ -3,4 +3,4 @@
 gnattest -P prj.gpr --gen-test-vectors --gen-test-binary --enum-strat --serialized-test-dir=bin-tests -q
 
 # Count the number of tests that we find, only 4 should appear
-find bin-tests -name 'pkg_ident_*' | wc -l
+find bin-tests -name 'ident-*' | wc -l
