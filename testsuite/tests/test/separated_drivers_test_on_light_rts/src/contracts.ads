baccf099529aa081f8e29eae54e17626a2b7bcec
Mathéo Rome <rome@adacore.com>
2026-05-26 13:55:00 +0000

    Prevent gnattest to run when using test separated drivers on a light rts

---
+++ testsuite/tests/test/separated_drivers_test_on_light_rts/src/contracts.ads
@@ -0,0 +1,16 @@
+with Simple;
+package Contracts is
+
+   function Sqrt (X : Float) return Float
+   with Test_Case => (Name => "test case 1",
+                      Mode => Nominal,
+                      Requires => X < 16.0,
+                      Ensures  => Sqrt'Result < 4.0),
+        Pre => (X >= 0.0);
+   pragma Post (Sqrt'Result >= 0.0);
+   pragma Test_Case (Name     => "test case 2",
+                     Mode     => Robustness,
+                     Requires => X < 0.0,
+                     Ensures  => Sqrt'Result = -1.0);
+
+end Contracts;
