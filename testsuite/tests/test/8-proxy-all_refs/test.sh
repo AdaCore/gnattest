a0ffc2f4e4da274837c520e576e0c34997334f21
Léo Creuse <creuse@adacore.com>
2026-08-06 08:03:44 +0000

    Testsuite: silence gprbuild invocation and update baselines after gpr2build update

--- testsuite/tests/test/8-proxy-all_refs/test.sh
+++ testsuite/tests/test/8-proxy-all_refs/test.sh
@@ -7,5 +7,5 @@ rm -r obj/gnattest/tests/
 
 # Run again with it, we should have no unsupported types this time.
 gnattest -P prj.gpr --gen-test-vectors --detect-tgen-proxies=all_refs
-gprbuild -P obj/gnattest/harness/test_driver.gpr
+gprbuild -P obj/gnattest/harness/test_driver.gpr -q
 ./obj/gnattest/harness/test_runner
