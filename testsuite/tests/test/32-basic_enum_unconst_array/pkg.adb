01d865b5922d2fca82eabf208a7cc952d0aa9379
Léo Creuse <creuse@adacore.com>
2026-09-11 10:54:25 +0200

    Tests/32-basic_enum: Split unconstrained array testing into different test
    This scenario requires allocating a large array (1000 elements) which is not
    really possible on embedded platforms. Splitting the test and only skipping
    the unconstrained array test on embedded platform still preserves some amount
    of validation.

---
+++ testsuite/tests/test/32-basic_enum_unconst_array/pkg.adb
@@ -0,0 +1,18 @@
+package body Pkg is
+
+   procedure Process (Int_Arr : Int_Arr_Type) is
+   begin
+      null;
+   end Process;
+
+   procedure Process (Disc_Rec_With_Constr : Disc_Rec_With_Constr_Type) is
+   begin
+      null;
+   end Process;
+
+   procedure Process (Rec_With_Constr_Rec : Rec_With_Constr_Rec_Type) is
+   begin
+      null;
+   end Process;
+
+end Pkg;
